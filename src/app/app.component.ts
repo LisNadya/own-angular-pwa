e1e7e876186af1aef35ca586e7a40615a7f9cdaf
lis.nadya <lis.nadya@avanade.com>
2021-11-01 19:12:59 +0800

    call mock api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
-
+import { AppService } from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,12 +8,19 @@ import { SwUpdate } from '@angular/service-worker';
 })
 export class AppComponent implements OnInit {
   title = 'angular-pwa-app';
-
-  constructor(private swUpdate: SwUpdate) {
+  users: any = [];
+  constructor(private swUpdate: SwUpdate, private injector: Injector) {
   }
 
   ngOnInit() {
-
+      this.injector.get(AppService).getUsers().subscribe(data => 
+        {
+          if(data){
+            console.log(data)
+            this.users = data;
+          }
+        }
+      )
       if (this.swUpdate.isEnabled) {
 
           this.swUpdate.available.subscribe(() => {
---
+++ src/app/app.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable({
+    providedIn: 'root'
+})
+export class AppService {
+    
+    constructor (protected httpClient: HttpClient){
+    }
+    public defaultHeaders = new HttpHeaders({
+        'Content-Type': 'application/json'
+    });
+    private httpOptions = {
+        headers: this.defaultHeaders,
+        observe: 'body' as 'body',
+        reportProgress: false
+    };
+    getUsers() {
+        const URL = `https://my.api.mockaroo.com/users.json?key=fc2ee410`;
+        return this.httpClient.get<any>(URL);
+    }
+}
